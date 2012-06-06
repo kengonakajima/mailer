67d1b5b7285ec031a8b3b4d9a3fc08ffeeebf6ab
kengo nakajima <kengo.nakajima@gmail.com>
2012-06-06 16:28:49 +0900

    worked

---
+++ web.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+# http to smtp mailer
+#
+require "rubygems"
+require "webrick"
+require "./rumino/rumino"
+
+PORT = 10080
+PIDFILE = nil
+EMAILTO = "kengo.nakajima@gmail.com"
+
+addconf=nil
+if ARGV.size == 1 then 
+  addconf = readJSON(ARGV[0])
+  PORT = addconf["webPort"]
+  PIDFILE = addconf["pidFile"]
+end
+
+$srv = WEBrick::HTTPServer.new({ 
+                                :BindAddress => "0.0.0.0",
+                                :Port => PORT 
+                              })
+
+$hist={}
+
+class Receiver < WEBrick::HTTPServlet::AbstractServlet
+  def do_POST(req,res)
+    path = req.path
+    data = req.query
+    p( path )
+    p( data )
+    if ! $hist[data] then
+      from = path.gsub("/","") + "@ringo.io"
+      subj = from
+      sendmail( from, EMAILTO, subj, data )      
+    else
+      p("skip dup entry")
+    end
+  end
+#  def do_GET(req,res)
+#    p( req.query )
+#  end
+end
+
+$srv.mount( "/", Receiver )
+
+
+
+def terminate()
+  cmd( "rm -f #{PIDFILE}")
+  $srv.shutdown
+end
+
+trap("INT"){terminate()}
+trap("TERM"){terminate()}
+
+savePid(PIDFILE)
+
+p "start server:", PORT
+$srv.start()
