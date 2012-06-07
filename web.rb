ff1652e03e9fc9317109ce94f4e3e1c0a356a4b3
kengo nakajima <kengo.nakajima@gmail.com>
2012-06-08 06:13:40 +0900

    miniwebize

--- web.rb
+++ web.rb
@@ -5,56 +5,29 @@ require "rubygems"
 require "webrick"
 require "./rumino/rumino"
 
-PORT = 38080
-PIDFILE = nil
 EMAILTO = "kengo.nakajima@gmail.com"
-
-addconf=nil
-if ARGV.size == 1 then 
-  addconf = readJSON(ARGV[0])
-  PORT = addconf["webPort"]
-  PIDFILE = addconf["pidFile"]
-end
-
-$srv = WEBrick::HTTPServer.new({ 
-                                :BindAddress => "0.0.0.0",
-                                :Port => PORT 
-                              })
 
 $hist={}
 
-class Receiver < WEBrick::HTTPServlet::AbstractServlet
-  def do_POST(req,res)
-    path = req.path
-    data = req.query
-    p( path )
-    p( data )
-    if ! $hist[data] then
-      from = path.gsub("/","") + "@ringo.io"
-      subj = from
-      sendmail( from, EMAILTO, subj, data )      
-    else
-      p("skip dup entry")
-    end
+web = MiniWeb.new()
+web.useConfJSONs( "defaults.json", ARGV[0] )
+web.useGlobalTrapAndPidFile()
+web.onPOST() do |req,res|
+  caption = req.path
+  caption.gsub!("/","")
+  caption = "crashreport" if caption.size == 0 
+
+  data = req.query
+  p( caption )
+  p( data )
+  if ! $hist[data] then
+    from = caption + "@ringo.io"         # URL path is used as mail subject and from address.
+    subj = caption
+    sendmail( from, EMAILTO, subj, data )      
+  else
+    p("skip dup entry")
   end
-#  def do_GET(req,res)
-#    p( req.query )
-#  end
 end
 
-$srv.mount( "/", Receiver )
 
-
-
-def terminate()
-  cmd( "rm -f #{PIDFILE}")
-  $srv.shutdown
-end
-
-trap("INT"){terminate()}
-trap("TERM"){terminate()}
-
-savePid(PIDFILE)
-
-p "start server:", PORT
-$srv.start()
+web.start()
